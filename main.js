83febeef953e486dc6f5a151635e8a0432ff6947
Alex <89929071+AlexBreskolenko@users.noreply.github.com>
2024-09-16 20:40:46 +0300

    Task 1 completed

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
+// **********************Task 1
+// Класс для сотрудника
+class Employee {
+  constructor(name) {
+    // Свойство имя сотрудника
+    this.name = name;
+  }
+  // Метод для вывода имя сотрудника
+  displayInfo() {
+    console.log(`Name : ${this.name}`);
+  }
+}
 
+// Класс менеджер,наследует класс Employee
+class Manager extends Employee {
+  constructor(name, department) {
+    super(name);
+    this.department = department;
+  }
+  displayInfo() {
+    console.log(`Name: ${this.name}\nDepartment: ${this.department}`);
+  }
+}
+
+const employee = new Employee("John Smith");
+employee.displayInfo();
+
+const manager = new Manager("Jone Doe", "Sales");
+manager.displayInfo();
